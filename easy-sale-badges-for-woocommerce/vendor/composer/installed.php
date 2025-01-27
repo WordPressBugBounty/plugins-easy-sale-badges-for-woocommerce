495fcc27a2622380753e13fa4bf3b2af2240ad06
Sean Murphy <sean@iamseanmurphy.com>
2025-01-27 22:18:05 +0000

    5.0.0

--- easy-sale-badges-for-woocommerce/vendor/composer/installed.php
+++ easy-sale-badges-for-woocommerce/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'asanaplugins/easy-sale-badges',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '42adb0cbe8f60c4ccf904a5381c2e19ac730c307',
+        'reference' => '03fb563c2612f69ac97864ed936ef910403de7c4',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'asanaplugins/easy-sale-badges' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '42adb0cbe8f60c4ccf904a5381c2e19ac730c307',
+            'reference' => '03fb563c2612f69ac97864ed936ef910403de7c4',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
